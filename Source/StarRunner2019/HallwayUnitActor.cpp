ca4983d215e996d553b5dae514d8435144287272
James Wu <therealjameswu@gmail.com>
2019-05-16 21:42:57 -0400

    Set static mesh as root component

--- Source/StarRunner2019/HallwayUnitActor.cpp
+++ Source/StarRunner2019/HallwayUnitActor.cpp
@@ -11,36 +11,25 @@ AHallwayUnitActor::AHallwayUnitActor()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
-	RootComponent = Root;
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("<Mesh>"));
-	Mesh->AttachToComponent(Root, FAttachmentTransformRules::KeepRelativeTransform);
+	// // Based on https://forums.unrealengine.com/development-discussion/c-gameplay-programming/118457-set-static-mesh-in-c
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("StaticMesh'/Game/Geometry/Meshes/hallwayunit.hallwayunit'"));
+	UStaticMesh *StaticMesh = MeshAsset.Object;
+	
+	MeshComponent->SetStaticMesh(StaticMesh);
+	// Assign the static mesh component to root
+	RootComponent = MeshComponent;
 }
 
 // Called when the game starts or when spawned
 void AHallwayUnitActor::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// Based on https://forums.unrealengine.com/development-discussion/c-gameplay-programming/1377430-oncomponenthit-isn-t-working
-	Mesh->OnComponentBeginOverlap.AddDynamic(this, &AHallwayUnitActor::OnBeginOverlap);
 }
 
 // Called every frame
 void AHallwayUnitActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-}
-
-// Based on https://answers.unrealengine.com/questions/789940/begin-overlap-in-c.html
-void AHallwayUnitActor::OnBeginOverlap(UPrimitiveComponent *OverlappedComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
-{
-	UE_LOG(LogTemp, Warning, TEXT("Help"));
-	// Other Actor is the actor that triggered the event. Check that is not ourself.
-	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr))
-	{
-		//doshit
-		UE_LOG(LogTemp, Warning, TEXT("WE IN BOIS"));
-	}
 }
--- Source/StarRunner2019/HallwayUnitActor.h
+++ Source/StarRunner2019/HallwayUnitActor.h
@@ -25,12 +25,6 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	UFUNCTION()
-	void OnBeginOverlap(UPrimitiveComponent *OverlappedComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
-
-	UPROPERTY()
-	USceneComponent *Root;
-
 	UPROPERTY(EditAnywhere)
-	UStaticMeshComponent *Mesh;
+	UStaticMeshComponent *MeshComponent;
 };
