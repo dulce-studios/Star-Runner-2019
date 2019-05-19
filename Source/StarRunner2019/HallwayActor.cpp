7a5432c3f35da04d7f1fc2bac560e2e3f019a7ae
James Wu <therealjameswu@gmail.com>
2019-05-19 13:51:33 -0400

    Add component spawn functionality

---
+++ Source/StarRunner2019/HallwayActor.cpp
@@ -0,0 +1,81 @@
+// Copyright 2019 Dulce Studios. All Rights Reserved.
+
+#include "HallwayActor.h"
+
+#include "Engine/Classes/Kismet/KismetSystemLibrary.h"
+
+// For constructing the hallways
+#include "HallwayJointComponent.h"
+#include "HallwayUnitComponent.h"
+
+#include <stdlib.h>
+
+// Sets default values
+AHallwayActor::AHallwayActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	USceneComponent *RootSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
+	RootComponent = RootSceneComponent;
+
+	Setup();
+}
+
+void AHallwayActor::Setup()
+{
+	UHallwayUnitComponent *InitialHallwayUnitComponent = CreateDefaultSubobject<UHallwayUnitComponent>(TEXT("InitialHallwayUnitComponent"));
+	InitialHallwayUnitComponent->AttachToComponent(GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+
+	FVector Origin;
+	FVector BoxExtent;
+	float SphereRadius;
+	UKismetSystemLibrary::GetComponentBounds(InitialHallwayUnitComponent, Origin, BoxExtent, SphereRadius);
+
+	float XLength = (BoxExtent.X * 2.0f);
+
+	FRotator Rotator(0.0f, 0.0f, 0.0f);
+	FVector Translation(XLength, 0.0f, 0.0f);
+	FVector Scale(1.0f, 1.0f, 1.0f);
+
+	// const TCHAR* ComponentName = *FString::Printf(TEXT("Yo_%d"), 50);
+
+	// UE_LOG(LogTemp, Warning, FString::Printf(TEXT("Bob %d"), 23));
+
+	int HallwayCount = rand() % 5 + 1;
+
+	UE_LOG(LogTemp, Warning, TEXT("Generating %d hallways"), HallwayCount);
+
+	int i;
+	for (i = 1; i < HallwayCount; ++i)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("In FOR LOOP"));
+		UE_LOG(LogTemp, Warning, TEXT("HallwayUnitComponent%d"), i);
+		UHallwayUnitComponent *NextHallwayUnitComponent = CreateDefaultSubobject<UHallwayUnitComponent>(FName(*FString::Printf(TEXT("HallwayUnitComponent%d"), i)));
+		FTransform HallwayUnitComponentTransform(Rotator, Translation, Scale);
+
+		NextHallwayUnitComponent->AttachToComponent(GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+		NextHallwayUnitComponent->SetRelativeTransform(HallwayUnitComponentTransform);
+
+		Translation.X += XLength;
+	}
+
+	UHallwayJointComponent *HallwayJointComponent = CreateDefaultSubobject<UHallwayJointComponent>(TEXT("HallwayJointComponent"));
+	FTransform HallwayJointComponentTransform(Rotator, Translation, Scale);
+
+	HallwayJointComponent->AttachToComponent(GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+	HallwayJointComponent->SetRelativeTransform(HallwayJointComponentTransform);
+	
+}
+
+// Called when the game starts or when spawned
+void AHallwayActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AHallwayActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/StarRunner2019/HallwayActor.h
@@ -0,0 +1,29 @@
+// Copyright 2019 Dulce Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HallwayActor.generated.h"
+
+UCLASS()
+class STARRUNNER2019_API AHallwayActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHallwayActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	void Setup();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
