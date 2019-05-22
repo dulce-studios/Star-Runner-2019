7484648c4cafd479d549d9d64fec0bb5877aed8c
theAmbientOrange <therealjameswu@gmail.com>
2019-05-21 20:56:23 -0400

    Update box trigger bounds

--- Source/StarRunner2019/HallwayJointComponent.cpp
+++ Source/StarRunner2019/HallwayJointComponent.cpp
@@ -12,8 +12,8 @@ UHallwayJointComponent::UHallwayJointComponent()
 	UBoxComponent *DeconstructorTriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("DeconstructorTriggerBox"));
 
 	FQuat Rotation(0.0, 0.0, 0.0, 0.0);
-	FVector Translation(0.0, 0.0, 1.0);
-	FVector Scale(0.025, 0.025, 0.025);
+	FVector Translation(0.0, 0.0, 2.0);
+	FVector Scale(0.075, 0.075, 0.075);
 
 	const FTransform DeconstructorTriggerBoxTransform(Rotation, Translation, Scale);
 
