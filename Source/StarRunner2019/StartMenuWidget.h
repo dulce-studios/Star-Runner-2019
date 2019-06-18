0e66a5b92deb1d8878fe82c3c72849f82037c433
theAmbientOrange <therealjameswu@gmail.com>
2019-06-17 21:31:54 -0400

    Add CreditsButton property

--- Source/StarRunner2019/StartMenuWidget.h
+++ Source/StarRunner2019/StartMenuWidget.h
@@ -18,5 +18,8 @@ public:
 	UButton* StartGameButton;
 
 	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* CreditsButton;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
 	UButton* ExitGameButton;
 };
